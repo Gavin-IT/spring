41b06a7f3a8073d16a548bd5852a4ec06abe538f
Gavin <it_gavin@163.com>
2018-04-17 23:10:23 +0800

    项目搭建

---
+++ spring-parent/spring-ioc/src/main/java/com/mjk/spring/ioc01/App.java
@@ -0,0 +1,17 @@
+package com.mjk.spring.ioc01;
+
+import lombok.Getter;
+import lombok.Setter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    @Getter @Setter private String userName;
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
